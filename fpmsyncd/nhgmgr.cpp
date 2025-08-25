e2997d68ba92779c63518a12b50dc59e678ab069
linsongnan <linsongnan.lsn@alibaba-inc.com>
2025-08-25 15:41:40 +0800

    [fpmsyncd]: add nhg table handling for rib/fib
    Signed-off-by: linsongnan <linsongnan.lsn@alibaba-inc.com>

--- fpmsyncd/nhgmgr.cpp
+++ fpmsyncd/nhgmgr.cpp
@@ -1,48 +1,256 @@
 #include "nhgmgr.h"
 
-
 NHGMgr::getInstance()
 {
     static NHGMgr instance;
     return &instance;
 }
+
 NHGMgr::~NHGMgr()
 {
 }
-int NHGMgr::reviceNewNHGFromRIB(const RibNHGContext* ribCtx)
+
+int NHGMgr::addNHG(const NextHopGroupFull nhg)
 {
-    int ret=0;
-    // add nhg to rib table
-    // add ngh to sonic table
-    // update rib_2_sonic map
+    int ret = 0;
+    string key = NexthopKey::getKeyFromNextHopGroupFull(nhg);
+    if (m_rib_nhg_table.isNhgExist(key))
+    {
+        RIBNhgEntry *entry = m_rib_nhg_table->getEntry(key);
+        m_rib_nhg_table.update(entry)
+    }
+    else
+    {
+        RIBNhgEntry *new_entry = new RIBNhgEntry(nhg);
+        ret = m_rib_nhg_table.insert(new_entry)
+    }
+    // srv6 sonic nhg create
     return ret;
 }
-int NHGMgr::reviceDelNHGFromRIB(const RIBNHGContext* ribCtx)
+
+int NHGMgr::delNHG(uint32_t id)
 {
-    int ret=0;
+    if (!m_rib_nhg_table.isNhgExist(key))
+    {
+        SWSS_LOG_ERROR("NextHop group id %d not found. Dropping the route %s", nhg_id, destipprefix);
+        return 0;
+    }
+
     // del nhg from sonic table
-    // del ngh from rib table
-    // update rib_2_sonic map
-    return ret;
+    return m_rib_nhg_table.delNhg(id);
 }
 
-RIBNHGContext* NHGMgr::getNhgContextByKey(string key)
+RIBNhgEntry *NHGMgr::getRIBNhgEntryByKey(std::string key)
 {
-   return m_rib_nhg_table->getNhgContextByKey(key);
+    return m_rib_nhg_table->getEntry(key);
 }
 
-RIBNHGContext* NHGMgr::getNhgContextByRIBID(uint32_t id)
+RIBNhgEntry *NHGMgr::getRIBNhgEntryByRIBID(uint32_t id)
 {
-   return m_rib_nhg_table->getNhgContextByRIBID(id);
+    return m_rib_nhg_table->getEntry(id);
 }
 
-RIBNHGContext* RIBNhgTable::getNhgContextByKey(string key)
+// TODO: add sonic boject creationuint32_t
+
+SonicNHGObject *NHGMgr::getSonicNhgByRIBID(string key)
 {
-   if m_key_2_id_map.find(key) != m_key_2_id_map.end()
-   {
-       uint32_t id = m_key_2_id_map[key];
-       return getNhgContextByRIBID(id);
-   }else{
-       return nullptr;
-   }
+    return nullptr;
 }
+
+SonicNHGObject *getSonicNhgByKey(string key)
+{
+    return nullptr;
+}
+
+RIBNhgEntry *RIBNhgTable::getEntry(std::string key)
+{
+    auto it = m_key_2_id_map.find(key);
+    if (it == m_key_2_id_map.end())
+    {
+        return nullptr;
+    }
+    it = m_nhg_map.find(it->second);
+    if (it == m_nhg_map.end())
+    {
+        return nullptr;
+    }
+    return it->second;
+}
+
+RIBNhgEntry *RIBNhgTable::getEntry(uint32_t id)
+{
+    auto it = m_nhg_map.find(id);
+    if (it == m_key_2_id_map.end())
+    {
+        return nullptr;
+    }
+    return it->second;
+}
+
+int RIBNhgTable::delNhg(uint32_t id)
+{
+    if (m_nhg_map.find(id) == m_nhg_map.end())
+    {
+        SWSS_LOG_ERROR("NextHop group id %d not found.", nhg_id);
+        return 0;
+    }
+
+    RIBEntry *entry = m_nhg_map[id];
+    if (entry->getDependentsID().size() != 0)
+    {
+        SWSS_LOG_ERROR("NextHop group id %d still has dependents.", nhg_id);
+        return -1;
+    }
+
+    m_key_2_id_map.erase(entry->getKey());
+    delete entry;
+    m_nhg_map.erase(id);
+    return 0;
+}
+
+int RIBNhgTable::addNhg(NextHopGroupFull nhg)
+{
+    if (nhg == nullptr)
+    {
+        SWSS_LOG_ERROR("nhg is null");
+        return -1;
+    }
+    if (m_nhg_map
+            .find(nhg->getId()) != m_nhg_map.end())
+    {
+        SWSS_LOG_ERROR("NextHop group id %d key %s already exists.", nhg->id);
+        return -1;
+    }
+
+    RIBEntry *entry = RIBEntry::create_nhg_entry(nhg);
+    if (entry == nullptr)
+    {
+        SWSS_LOG_ERROR("Failed to create nhg entry for %s", nhg->getKey());
+        return -1;
+    }
+
+    update_nhg_dependents(entry)
+
+        m_nhg_map.insert(std::make_pair(nhg->getId(), entry));
+    m_key_2_id_map.insert(std::make_pair(entry->getKey(), entry));
+    return 0;
+}
+
+void RIBNhgTable::add_nhg_dependents(RIBNhgEntry *entry)
+{
+    for (int i = 0; i < entry->nhg.group.size(); i++)
+    {
+        m_nhg_map[entry->nhg.group[i].first]->m_depends.push_back(entry->nhg.getId());
+    }
+}
+
+void RIBNhgTable::remove_nhg_dependents(RIBNhgEntry *entry)
+{
+    for (int i = 0; i < entry->nhg.group.size(); i++)
+    {
+        m_nhg_map[entry->nhg.group[i].first]->m_depends.push_back(entry->nhg.getId());
+    }
+}
+
+RIBNhgEntry *RIBNhgEntry::create_nhg_entry(const NextHopGroupFull nhg)
+{
+
+    RIBNhgEntry *entry = new RIBNhgEntry();
+    int ret = entry->setEntry(nhg) if ret != 0
+    {
+        delete entry;
+        return nullptr;
+    }
+    return entry;
+}
+
+vector<pair<uint32_t, uint8_t>> RIBNhgEntry::getGroup()
+{
+    return m_group;
+}
+
+vector<pair<uint32_t, uint8_t>> RIBNhgEntry::getResolvedGroup()
+{
+    return m_resolved_group;
+}
+
+vector<RIBNhgEntry *> RIBNhgEntry::~getDependsID()
+{
+    return m_depends;
+}
+
+vector<RIBNhgEntry *> RIBNhgEntry::getDependentsID()
+{
+    return m_dependents;
+}
+
+int setEntry(NexthopGroupFull)
+{
+    entry->key = NexthopKey(&nhg) for (int i = 0; i < nhg.group.size(); i++)
+    {
+
+        // validate group member
+        if (m_map.find(nhg.group[i].first) == m_map.end())
+        {
+            SWSS_LOG_ERROR("NextHop id %d in group not found.", nhg_id);
+            return -1;
+        }
+
+        // update resolved group
+        if (nhg
+                .group[i]
+                .type != nexthop_types_t::NEXTHOP_TYPE_RECURSIVED)
+        {
+            entry->resolved_group.push_back(nhg.group[i]);
+        }
+
+        // TODO: add dependent and depends
+    }
+    return 0;
+}
+
+SonicNHGTable::SonicNHGTable(DBConnector &db)
+{
+    m_db = db;
+}
+
+SonicNHGTable::~SonicNHGTable()
+{
+    return;
+}
+
+int SonicNHGTable::addNhg()
+{
+    return 0;
+}
+
+int SonicNHGTable::delNhg()
+{
+    return 0;
+}
+
+SonicNHGEntry *SonicNHGTable::getEntry(std::string key)
+{
+    return nullptr;
+}
+
+SonicNHGEntry *SonicNHGTable::getEntry(uint32_t id)
+{
+    return nullptr;
+}
+
+SonicNHGEntry::SonicNHGEntry()
+{
+    return;
+}
+
+SonicNHGEntry::~SonicNHGEntry()
+{
+    return;
+}
+
+void SonicNHGEntry::create_nhg_entry()
+{
+    return;
+}
+
--- fpmsyncd/nhgmgr.h
+++ fpmsyncd/nhgmgr.h
@@ -1,142 +1,116 @@
-//
-// Created by lsn on 25-8-11.
-//
 
 #ifndef NHGMGR_H
 #define NHGMGR_H
 
 #include "dbconnector.h"
-#include<string.h>
-
-// For now
-#define DEFAULT_RIB_TABLE_SIZE 1000
-#define DEFAULT_SONIC_TABLE_SIZE 1000
+#include <string.h>
 
 using namespace std;
 
-class RIBNHGContext{
-};
-class SonicNHGObject{
-};
-
-class NHGMgr {
+class NHGMgr
+{
 
 public:
     NHGMgr();
     ~NHGMgr();
-    static NHGMgr* getInstance();
-    int reviceNewNHGFromRIB(const RIBNHGContext*);
-    int reviceDelNHGFromRIB(const RIBNHGContext*);
-    RIBNHGContext* getNhgContextByKey(std::string key);
-    RIBNHGContext* getNhgContextByRIBID(uint32_t id);
-    void getSonicNhgBySonicID(uint32_t);
-    SonicNHGObject* getSonicNhgByRIBID(string key);
-    SonicNHGObject* getSonicNhgByRIBID(string key);
-    SonicNHGObject* getSonicNhgByKey(string key);
+    static NHGMgr *getInstance();
+    int addNHG(NextHopGroupFull nhg);
+    // int addNHGFromRIB(uint32_t id, const string gateway, const int32_t ifindex);
+    int delNHG(uint32_t id);
+    RIBNhgEntry *getRIBNhgEntryByKey(std::string key);
+    RIBNhgEntry *getRIBNhgEntryByRIBID(uint32_t id);
+    SonicNHGObject *getSonicNHGByRIBID(string key);
+    SonicNHGObject *getSonicNHGByKey(string key);
 
 private:
-    DBConnector* m_db;
+    DBConnector *m_db;
     RIBNhgTable m_rib_nhg_table;
-    SonicNhgTable m_sonic_nhg_table;
-    map<uint32_t, uint32_t> m_sonic_2_rib_map;
-    map<uint32_t, uint32_t> m_rib_2_sonic_map;
+    SonicNHGTable m_sonic_nhg_table;
 };
 
-class RIBNhgTable {
+class RIBNhgTable
+{
 
 public:
-    RIBNhgTable(DBConnector& db);
+    RIBNhgTable(DBConnector &db);
     ~RIBNhgTable();
-    int addNhg();
-    int delNhg();
-    vector<RIBNHGContext*> getDepends(std::string key);
-    vector<RIBNHGContext*> getDependents(uint64_t id);
-    RIBNHGContext* getNhgContext(uint64_t id);
-
+    int addNhg(NextHopGroupFull nhg);
+    int delNhg(uint32_t id);
+    vector<RIBNhgEntry *> getDepends(std::string key);
+    vector<RIBNhgEntry *> getDependents(uint32_t id);
+    RIBNhgEntry *getEntry(uint32_t id);
+    RIBNhgEntry *getEntry(std::string key);
+    void add_nhg_dependents(RIBNhgEntry *entry);
+    void remove_nhg_dependents(RIBNhgEntry *entry);
     bool isNhgExist(string key);
-    bool isNhgExist(uint64_t id);
-    vector<RIBNHGContext*> getNhgByID(std::string key);
+    bool isNhgExist(uint32_t id);
 
 private:
-    DBConnector* m_db;
-    map<uint64_t, RIBNhgEntry*> m_nhg_map;
-    map<string, uint64_t> m_key_2_id_map;
+    NexthopKey m_key;
+    map<uint32_t, RIBNhgEntry *> m_nhg_map;
+    map<string, uint32_t> m_key_2_id_map;
 }
 
-class NexthopKey {
+class RIBNhgEntry
+{
+public:
+    RIBNhgEntry();
+    vector<RIBNhgEntry *> getDependsID();
+    vector<RIBNhgEntry *> getDependentsID();
+    vector<pair<uint32_t, uint8_t>> getGroup();
+    vector<pair<uint32_t, uint8_t>> getResolvedGroup();
+    int setEntry(NexthopGroupFull);
+    static RIBNhgEntry *create_nhg_entry(NextHopGroupFull);
+void setKey(NexthopKey) private : NexthopKey m_key;
+    NextHopGroupFull m_nhg;
+    vector<pair<uint32_t, uint8_t>> m_group;
+    vector<pair<uint32_t, uint8_t>> m_resolved_group;
+    vector<uint32_t> m_depends;
+    vector<uint32_t> m_dependents;
+    // vector<uint32_t> m_sonic_object_id;
+}
+
+// pic
+class SonicNHGTable
+{
+
+public:
+    SonicNHGTable(DBConnector &db);
+    ~SonicNHGTable();
+    int addNhg();
+    int delNhg();
+    SonicNHGEntry *getEntry(std::string key);
+    SonicNHGEntry *getEntry(uint32_t id);
+
+private:
+    DBConnector *m_db;
+}
+
+class SonicNHGEntry
+{
+
+public:
+    SonicNHGEntry();
+    void create_nhg_entry();
+
+private:
+    NexthopKey m_key;
+}
+
+class NexthopKey
+{
 
 public:
     NexthopKey();
+    NexthopKey(const NextHopGroupFull *nhg) {
+
+    };
     string nexthop;
     string intf;
-    vector<pair<uint32_t,uint32_t>> group;
+    vector<pair<uint32_t, uint32_t>> group;
+    vector<pair<uint32_t, uint32_t>> flags;
     string getNhgKey() {
-        if length(group) <= 0 {
-            return nexthop + "|" + intf;
-        }else{
-            string key = "";
-            for (auto it = group.begin(); it != group.end(); ++it) {
-                key += it->first + "|" + it->second + "|";
-            }
-            return key;
-        }
+
     };
-    NexthopKey createNexthopKeyFromRIBCtx(const RIBNHGContext*) {
-        return "";
-    }
 }
-
-
-class RIBNhgEntry {
-
-public:
-    RIBNhgEntry();
-    vector<RIBNhgEntry*> getDependsID();
-    vector<RIBNhgEntry*> getDependentsID();
-
-private:
-    RIBNHGContext m_nhg_ctx;
-    vector<pair<uint32_t,uint32_t>> m_depends;
-    vector<pair<uint32_t,uint32_t>> m_dependents;
-    void create_nhg_entry(string key, void* nhg);
-}
-
-class SonicNhgEntry {
-
-public:
-    SonicNhgEntry();
-    void create_nhg_entry(string key, void* nhg);
-
-private:
-    vector<pair<uint32_t,uint32_t>> m_depends;
-    vector<pair<uint32_t,uint32_t>> m_dependents;
-}
-
-class SonicNhgTable {
-
-public:
-    SonicNhgTable(DBConnector& db);
-    ~SonicNhgTable();
-    void addNhg();
-    void delNhg();
-    void getNhgByKey(std::string key, void* nhg);
-    void getNhgByID(string key, void* nhg);
-
-private:
-    DBConnector* m_db;
-    void write_to_db();
-}
-
-class SonicNhgEntry {
-
-public:
-    SonicNhgEntry();
-    void create_nhg_entry();
-    vector<SonicNhgEntry*> getDependsID();
-    vector<SonicNhgEntry*> getDependentsID();
-
-private:
-    vector<pair<uint32_t,uint32_t>> m_depends;
-    vector<pair<uint32_t,uint32_t>> m_dependents;
-}
-#endif //NHGMGR_H
+#endif // NHGMGR_H
